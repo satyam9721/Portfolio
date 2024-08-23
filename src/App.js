05c1e1d152510231de90272e9669d29319ca2379
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2024-08-23 16:03:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jija ji ka hotel hai
         </p>
     <h3>Techy Satyam</h3>
         <a
