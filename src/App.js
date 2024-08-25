240f4b00c22c9807ca7d8a73ba834b92b9493c98
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2024-08-25 10:39:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jija ji ka hotel hai
+          rajeev madarchod hai
         </p>
     <h3>Techy Satyam</h3>
         <a
